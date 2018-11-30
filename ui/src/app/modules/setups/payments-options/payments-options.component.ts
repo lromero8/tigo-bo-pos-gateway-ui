8743b212d36968d8dbbebfffdaf2d5c793894bdb
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2018-11-30 09:03:44 -0600

    link services to the setups components

--- ui/src/app/modules/setups/payments-options/payments-options.component.ts
+++ ui/src/app/modules/setups/payments-options/payments-options.component.ts
@@ -8,6 +8,7 @@
 
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { PaymentOptionsService } from '../../../services/payment-options.service'
 
 @Component({
   selector: 'app-payments-options',
@@ -17,7 +18,8 @@ import { Title } from '@angular/platform-browser';
 })
 export class PaymentsOptionsComponent implements OnInit {
 
-  constructor(private titleService: Title) {
+  constructor(private titleService: Title,
+              private paymentOptionsService: PaymentOptionsService) {
     
   }
 
