65f74c4efc53b7250d6e4a2fdfc22bd4b6823c27
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2018-12-04 13:58:30 -0600

    working in regular expressions

--- ui/src/app/modules/setups/regular-expressions/regular-expressions.component.ts
+++ ui/src/app/modules/setups/regular-expressions/regular-expressions.component.ts
@@ -9,6 +9,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { RegularExpressionsService } from '../../../services/regular-expressions.service'
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-regular-expressions',
@@ -18,12 +19,27 @@ import { RegularExpressionsService } from '../../../services/regular-expressions
 })
 export class RegularExpressionsComponent implements OnInit {
 
+  public dataRetrived: Array<any>;
+  public checkboxGroupForm: FormGroup;
   constructor(private titleService: Title,
-              private regularExpressionsService: RegularExpressionsService) { 
+              private regularExpressionsService: RegularExpressionsService, 
+              private formBuilder: FormBuilder) {
+    this.regularExpressionsService.retrieve().subscribe(
+      data => {
+        this.dataRetrived = data;
+      },
+      error => {
+
+      }
+    ) 
   }
   
   ngOnInit() {
     this.setTitle('Expresiones regulares');
+    this.checkboxGroupForm = this.formBuilder.group({
+      left: true,
+      middle: false
+    });
   }
 
   public setTitle( newTitle: string): void {
